f6673783c7295244033f4e946d12ab0371201011
Arun198611 <arunsethu1986@gmail.com>
2025-10-08 17:57:34 +0530

    br24

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -3,7 +3,7 @@ it('passes', () => {
 
 cy.request('GET', 'https://formatjsononline.com/api/users/usr_1').then((response) => {
       expect(response.status).to.eq(200);
-      expect(response.duration).to.be.below(1000);
+      expect(response.duration).to.be.below(5000);
       const ResponseTime=response.duration;
       cy.log(ResponseTime);
 })
